4d3b614c3c0040a59d9989435e4b44322953e32b
Anton <64436397+shvenant@users.noreply.github.com>
2021-06-23 21:57:47 +0300

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -104,3 +104,13 @@ if (animItems.length > 0) {
 		animOnScroll();
 	}, 400);
 }
+
+
+
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 500);
+}
